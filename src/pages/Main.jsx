416a4954d8e83844b510004c453b5e6acac13a13
allenchenqaq <92569740+allenchenqaq@users.noreply.github.com>
2024-03-23 14:25:15 -0700

    creating main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,28 @@
 import React, { useState } from 'react'
 import Test from '../components/Test'
+import '../App.css'
 
 const Main = () => {
-
     return (
-        <Test />
-    )
-
+        <div className="meeting-full-page">
+            <div className="meeting-header">
+            <h1>Study Buddy</h1>
+            <p>Welcome to Study Buddy! Join or host a meeting!</p>
+            </div>
+            <div className="meeting-options">
+            <div className="meeting-join">
+                <h2>Join.</h2>
+                <p>Join a meeting</p>
+                <button className="btn join">Join</button>
+            </div>
+            <div className="meeting-host">
+                <h2>Host.</h2>
+                <p>Host a meeting</p>
+                <button className="btn host">Host</button>
+            </div>
+            </div>
+        </div>
+        );
 }
 
 export default Main;
